2d28215801148757458edc1664182ee989197f99
Joey <driscoll.recording@gmail.com>
2019-09-11 20:02:05 -0500

    added FavoritesController

---
+++ DigiPax/Controllers/FavoritesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DigiPax.Data;
+using DigiPax.Models;
+
+namespace DigiPax.Controllers
+{
+    public class FavoritesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FavoritesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Favorites
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Favorite.Include(f => f.Sample);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Favorites/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var favorite = await _context.Favorite
+                .Include(f => f.Sample)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (favorite == null)
+            {
+                return NotFound();
+            }
+
+            return View(favorite);
+        }
+
+        // GET: Favorites/Create
+        public IActionResult Create()
+        {
+            ViewData["SampleId"] = new SelectList(_context.Sample, "Id", "ApplicationUserId");
+            return View();
+        }
+
+        // POST: Favorites/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserId,SampleId")] Favorite favorite)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(favorite);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SampleId"] = new SelectList(_context.Sample, "Id", "ApplicationUserId", favorite.SampleId);
+            return View(favorite);
+        }
+
+        // GET: Favorites/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var favorite = await _context.Favorite.FindAsync(id);
+            if (favorite == null)
+            {
+                return NotFound();
+            }
+            ViewData["SampleId"] = new SelectList(_context.Sample, "Id", "ApplicationUserId", favorite.SampleId);
+            return View(favorite);
+        }
+
+        // POST: Favorites/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,SampleId")] Favorite favorite)
+        {
+            if (id != favorite.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(favorite);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FavoriteExists(favorite.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SampleId"] = new SelectList(_context.Sample, "Id", "ApplicationUserId", favorite.SampleId);
+            return View(favorite);
+        }
+
+        // GET: Favorites/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var favorite = await _context.Favorite
+                .Include(f => f.Sample)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (favorite == null)
+            {
+                return NotFound();
+            }
+
+            return View(favorite);
+        }
+
+        // POST: Favorites/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var favorite = await _context.Favorite.FindAsync(id);
+            _context.Favorite.Remove(favorite);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FavoriteExists(int id)
+        {
+            return _context.Favorite.Any(e => e.Id == id);
+        }
+    }
+}
